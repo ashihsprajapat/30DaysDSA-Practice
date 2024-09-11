98415c7d2fb15871ac7584c396e3fbd65888ba11
ashish prajapat <ashishprajapat507@gmail.com>
2024-09-11 19:43:46 +0530

    code of Bubble Sort

--- SortingDay2/BubbleSort.java
+++ SortingDay2/BubbleSort.java
@@ -1,8 +1,42 @@
 package SortingDay2;
 
 class BubbleSort {
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "   ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    arr[i] = arr[i] + arr[j];
+                    arr[j] = arr[i] - arr[j];
+                    arr[i] = arr[i] - arr[j];
+                }
+            }
+        }
+    }
+
+    public static void bubbleSort2(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j] + arr[j + 1];
+                    arr[j + 1] = arr[j] - arr[j + 1];
+                    arr[j] = arr[j] - arr[j + 1];
+                }
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        int arr[] = { 9, 3, 1, 0, 4, 2, 6, 8 };
+        print(arr);
+        bubbleSort2(arr);
+        print(arr);
     }
 }
